1d2be9f15aff7984d81d2da1b350b3c9bb7c771a
vheekey <vicformidable@gmail.com>
2026-02-05 03:51:09 +0100

    Files initialisation

---
+++ src/test/java/com/example/employee_weather_api/EmployeeWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_weather_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWeatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
